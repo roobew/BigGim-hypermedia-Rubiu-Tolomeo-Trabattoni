4793409bdcf1e6118f6ea8d1f985893f0137c68a
roobew <simone.rubiu91@gmail.com>
2015-05-27 16:53:52 +0200

    script.js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(ready);
+
+function ready() {
+    console.log("I'm ready!");
+    var id=0;
+
+ $.ajax({
+
+        method: "POST",
+        //dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        url: "http://hypermediabiggym.altervista.org/tmp.php", //Relative or absolute path to file.php file
+        data: {dato:id},
+        success: function(response) {
+            console.log(JSON.parse(response));
+
+            var inst=JSON.parse(response);
+            var el="";
+            for(var i=0;i<inst.length;i++){
+                console.log(inst[i].title);
+
+                el+=inst[i].id+" <b>"+inst[i].name+"</b>, "+inst[i].surname+"<br>";
+            }
+
+            $("stocazzo").html(el);
+        },
+        error: function(request,error)
+        {
+            alert(error);
+            console.log("Error");
+        }
+    });
+}
