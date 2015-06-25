a5fb2a1c85eaff628d2b8f2e8ec452c9da93aabf
roobew <simone.rubiu91@gmail.com>
2015-06-25 19:22:44 +0200

    course modifiction

--- js/getCourses.js
+++ js/getCourses.js
@@ -9,3 +9,39 @@ app.controller("CoursesCtrl", function($scope, $http) {
       // log error
     });
 });
+
+
+
+$(document).ready(ready);
+
+function ready() {
+    console.log("I'm ready!");
+    //var id=1;
+
+
+ $.ajax({
+        method: "POST",
+        crossDomain: true, //localhost purposes
+        url: "http://hypermediabiggym.altervista.org/getCourse.php", //Relative or absolute path to file.php file
+     data: { 'dato' : '1' },
+        success: function(response) {
+                console.log(JSON.parse(response));
+                var location=JSON.parse(response);
+                $(".courseName").html(location[0].nome);
+                $(".descr").html(location[0].description);
+                $("#img1").attr("src" , location[0].img1);
+                $("#img2").attr("src" , location[0].img2);
+                $("target").html(location[0].target);
+                $("level").html(location[0].level);
+                $("spec").html(location[0].specialFeatures);
+                $("#img3").attr("src" , location[0].img3);
+
+
+        },
+        error: function(request,error)
+        {
+            alert(error);
+            console.log("Error");
+        }
+    });
+}
