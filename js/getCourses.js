a5120c8c5d58a598b6ee495605439e36d34d7f48
roobew <simone.rubiu91@gmail.com>
2015-06-26 03:33:29 +0200

    madonne volanti

--- js/getCourses.js
+++ js/getCourses.js
@@ -3,27 +3,48 @@ var app = angular.module("Courses", []);
 app.controller("CoursesCtrl", function($scope, $http) {
   $http.get("http://hypermediabiggym.altervista.org/getCourses.php").
     success(function(data, status, headers, config) {
-      $scope.courses = data;
+      $scope.coursesCat = data;
     }).
     error(function(data, status, headers, config) {
       // log error
     });
 });
 
+function getUrlParameters(parameter, staticURL, decode){
+   /*
+    Function: getUrlParameters
+    Description: Get the value of URL parameters either from
+                 current URL or static URL
+    Author: Tirumal
+    URL: www.code-tricks.com
+   */
+   var currLocation = (staticURL.length)? staticURL : window.location.search,
+       parArr = currLocation.split("?")[1].split("&"),
+       returnBool = true;
 
+   for(var i = 0; i < parArr.length; i++){
+        parr = parArr[i].split("=");
+        if(parr[0] == parameter){
+            return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+            returnBool = true;
+        }else{
+            returnBool = false;
+        }
+   }
+
+   if(!returnBool) return false;
+}
 
 $(document).ready(ready);
 
 function ready() {
     console.log("I'm ready!");
-    //var id=1;
-
-
+    var idCourse =  getUrlParameters("idCourse", "", true);
  $.ajax({
         method: "POST",
         crossDomain: true, //localhost purposes
         url: "http://hypermediabiggym.altervista.org/getCourse.php", //Relative or absolute path to file.php file
-     data: { 'dato' : '1' },
+     data: { 'dato' : idCourse},
         success: function(response) {
                 console.log(JSON.parse(response));
                 var location=JSON.parse(response);
